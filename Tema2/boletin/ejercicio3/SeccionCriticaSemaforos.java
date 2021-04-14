d7ccb6833472b59bafaa8b5d5ea3c3901cdf045a
Rack <ragontru25@gmail.com>
2021-04-14 09:58:23 +0200

    Ejercicios T2
    Hasta el 5

--- Tema2/boletin/ejercicio3/SeccionCriticaSemaforos.java
+++ Tema2/boletin/ejercicio3/SeccionCriticaSemaforos.java
@@ -1,5 +1,16 @@
 package ejercicio3;
 
+/*
+ * Los semáforos se pueden utilizar para controlar el acceso a un 
+ * determinado recurso formado por un número finito de instancias. 
+ * Un semáforo se representa como una variable entera donde su valor 
+ * representa el número de instancias libres o disponibles en el 
+ * recurso compartido y una cola donde se almacenan los procesos o 
+ * hilos bloqueados esperando para usar el recurso En la fase de 
+ * inicialización, se proporciona un valor inicial al semáforo 
+ * igual al número de recursos inicialmente disponibles.
+ */
+
 import java.util.concurrent.Semaphore;
 
 public class SeccionCriticaSemaforos {
@@ -9,6 +20,20 @@ public class SeccionCriticaSemaforos {
 
 	public static void main(String[] args) {
 		int nSum = Integer.parseInt(args[0]);
+		sumadores = new Sumador[nSum];
+		for (int i = 0; i < nSum; i++) {
+			sumadores[i] = new Sumador(1000, i, semaphore);
+			sumadores[i].start();
+		}
+
+		for (int i = 0; i < nSum; i++) {
+			try {
+				sumadores[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Acumulador " + Acumula.acumulador);
 	}
 
 }
