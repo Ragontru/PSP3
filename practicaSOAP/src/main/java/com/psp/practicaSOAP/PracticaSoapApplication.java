b450904797cade5c3d8dba5a81a7ad6d0530b615
Rack <ragontru25@gmail.com>
2021-05-28 20:07:09 +0200

    Inicio SOAP

---
+++ practicaSOAP/src/main/java/com/psp/practicaSOAP/PracticaSoapApplication.java
@@ -0,0 +1,13 @@
+package com.psp.practicaSOAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSoapApplication.class, args);
+	}
+
+}
---
+++ practicaSOAP/src/test/java/com/psp/practicaSOAP/PracticaSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psp.practicaSOAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
